97319c28b939ff3b3e4011d4950336b841a55f56
leeyulgok <dbfrhr20@gmail.com>
2023-10-07 13:44:37 +0900

    S 찾기 게임: 실행

--- etc/findS.js
+++ etc/findS.js
@@ -18,12 +18,17 @@ function shuffleArray(board) {
   return board;
 }
 
+function playGame(userInputs) {
+  const board = createBoard();
+  return userInputs.map((input) => board[input - 1]);
+}
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("1~25사이의 숫자 중 3개를 입력하세요.", (answer) => {
+rl.question("1~25사이의 숫자 중 3개를 입력하세요. ", (answer) => {
   const userNumbers = answer.split(" ").map(Number);
   const result = playGame(userNumbers);
   console.log(`결과: ${result.join(", ")}`);
