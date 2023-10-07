0970c1ddbc299278aceec4ae02e1b03a85516c9d
leeyulgok <dbfrhr20@gmail.com>
2023-10-07 13:41:49 +0900

    S 찾기 게임: 보드판 만들고 섞기

--- etc/findS.js
+++ etc/findS.js
@@ -1,5 +1,23 @@
 const readline = require("readline");
 
+function createBoard() {
+  const initailChars = [
+    "S",
+    ...Array(3).fill("A"),
+    ...Array(7).fill("B"),
+    ...Array(14).fill("C"),
+  ];
+  return shuffleArray(initailChars);
+}
+
+function shuffleArray(board) {
+  for(let i = board.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [board[i], board[j]] = [board[j], board[i]];
+  }
+  return board;
+}
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
