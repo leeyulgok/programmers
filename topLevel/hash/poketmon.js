0e210a6e0843c9d9cc89f9fbe1f82334b7a61069
leeyulgok <dbfrhr20@gmail.com>
2023-10-12 17:31:40 +0900

    폰켓몬

---
+++ topLevel/hash/poketmon.js
@@ -0,0 +1,17 @@
+// 폰켓몬
+//
+
+function solution(nums) {
+  const takeNum = nums.length / 2;
+  let hashMap = {};
+  
+  for (const poketmon of nums) {
+    hashMap[poketmon] = true;
+  }
+
+  const typeCount = Object.keys(hashMap).length;
+
+  return Math.min(typeCount, takeNum);
+}
+
+console.log(solution([3, 3, 3, 2, 2, 4]));
