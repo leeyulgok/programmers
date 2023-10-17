e94629330a789ae3438b8bf5d857026b6ce84cf1
leeyulgok <dbfrhr20@gmail.com>
2023-10-17 16:33:55 +0900

    JadenCase 문자열 처리: 효율적인 풀이

---
+++ lv.2/jadenCase.js
@@ -0,0 +1,26 @@
+// JadenCase 문자열 만들기
+// JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다.
+// 단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
+// 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+function solution(s) {
+  let answer = s.split(" ");
+  let newArr = [];
+
+  for (const str of answer) {
+    let strArr = str.split("");
+    for (let i = 0; i < strArr.length; i++) {
+      if (i === 0) {
+        strArr[i] = strArr[i].toUpperCase();
+      } else {
+        strArr[i] = strArr[i].toLowerCase();
+      }
+    }
+    newArr.push(strArr.join(""));
+  }
+
+  return newArr.join(" ");
+}
+
+console.log(solution("3people unFollowed me"));
+console.log(solution("for the last week"));
