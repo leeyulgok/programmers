85c744ea5d2866d14254b26fc3cbdef8a562c50e
leeyulgok <dbfrhr20@gmail.com>
2023-11-20 16:31:05 +0900

    효율적인 풀이

--- lv.2/cutArray.js
+++ lv.2/cutArray.js
@@ -32,3 +32,16 @@ function solution(n, left, right) {
 
 console.log(solution(3, 2, 5));
 console.log(solution(4, 7, 14));
+
+// 효율적인 풀이
+function solution(n, left, right) {
+  let answer = [];
+
+  for (let i = left; i <= right; i++) {
+    let row = Math.floor(i / n);
+    let col = i % n;
+    answer.push(Math.max(row + 1, col + 1));
+  }
+
+  return answer;
+}
